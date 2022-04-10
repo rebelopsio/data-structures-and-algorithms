bae2d1bc02e55325c0923384356cd815614e8790
Stephen Morgan <steve@rebelops.io>
2022-04-10 09:49:54 -0700

    added golang and python solutions for binary search tree traversal
    Signed-off-by: Stephen Morgan <steve@rebelops.io>

---
+++ algoexpert/binary-search-trees/golang/binary-search-tree-traversal/main.go
@@ -0,0 +1,33 @@
+// Time = O(N)
+//  Space = O(N)
+
+package main
+
+import "fmt"
+
+type BST struct {
+	Value int
+
+	Left  *BST
+	Right *BST
+}
+
+func (tree *BST) InOrderTraverse(array []int) []int {
+	// Write your code here.
+	return nil
+}
+
+func (tree *BST) PreOrderTraverse(array []int) []int {
+	// Write your code here.
+	return nil
+}
+
+func (tree *BST) PostOrderTraverse(array []int) []int {
+	// Write your code here.
+	return nil
+}
+
+func main() {
+	fmt.Println(temp_bst)
+	fmt.Println(InOrd)
+}
